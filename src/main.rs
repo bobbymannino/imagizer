a80b1b1277e37f05f9249764526a256b3137ac72
Bob Mannino <manninobobby@icloud.com>
2025-06-30 21:13:22 +0100

    add empty api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Imagizer")
+use axum::{Router, middleware, routing::get};
+use dotenv::dotenv;
+use std::env;
+use tokio::net::TcpListener;
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let app = Router::new()
+    let port = env::var("PORT").unwrap_or_else(|_| "1345".to_string());
+    let port: u16 = match port.parse() {
+        Ok(port) => port,
+        Err(_) => panic!("PORT is invalid"),
+    };
+
+    // TODO check if port is in use
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", port))
+        .await
+        .unwrap();
+
+    println!("Server listening on port {}", port);
+
+    axum::serve(listener, app).await.unwrap();
 }
